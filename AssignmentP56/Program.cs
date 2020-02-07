1cdf8dfdae912dd6d72f9d5a404c820eb0290640
T33mac <31869841+T33mac@users.noreply.github.com>
2020-02-07 14:52:31 -0800

    Add files via upload

--- AssignmentP56/Program.cs
+++ AssignmentP56/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Welcome to Acme Accounting Systems\nRemember, we're counting on you!");
+        Console.WriteLine("Welcome to Acme Accounting Systems\nRemember, we're \"accounting\" on you!");
         Console.ReadLine();
     }
 }
