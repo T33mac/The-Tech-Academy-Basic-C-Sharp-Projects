067af87c25d124a78dc9fd917c618ab871e8b691
T33mac <31869841+T33mac@users.noreply.github.com>
2020-02-08 17:37:18 -0800

    add LMS p. 101
    add LMS p. 101

---
+++ InsuranceApproval/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your age?");
+        string age = Console.ReadLine();
+        int Age = Convert.ToInt32(age);
+        Console.WriteLine("Have you ever had a DUI?(Answer \"true\" or \"false\")");
+        string dui = Console.ReadLine();
+        bool DUI = Convert.ToBoolean(dui);
+        Console.WriteLine("How many speeding tickets do you have?");
+        string tickets = Console.ReadLine();
+        int Tickets = Convert.ToInt32(tickets);
+
+        bool isQualified = (Age > 15 && DUI == false && Tickets <= 3);
+        Console.WriteLine("Qualified?");
+        Console.WriteLine(isQualified);
+        Console.ReadLine();
+    }
+}
+
