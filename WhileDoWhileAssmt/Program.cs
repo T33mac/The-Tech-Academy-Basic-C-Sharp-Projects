ae174398cb958b0629b32ffa666ced9250b318d2
T33mac <31869841+T33mac@users.noreply.github.com>
2020-02-13 10:04:48 -0800

    add lms p.126 assignment
    add lms p.126 assignment

---
+++ WhileDoWhileAssmt/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Rate how you're feeling from 1-100");
+        int number = Convert.ToInt32(Console.ReadLine());
+        bool happy = number >= 80;
+        bool okay = number > 64 && number < 80;
+        bool beenBetter = number > 29 && number < 65;
+        bool miserable = number < 30;
+        
+        while (happy)
+        {
+            Console.WriteLine("Your a Happy boy/girl!");
+            Console.Read();
+            break;
+        }
+
+        while (beenBetter)
+        {
+            do
+            {
+                switch (number)
+                {
+                    case 50:
+                        Console.WriteLine("Wow! right in the middle!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Buck up, little Buckaroo!");
+                        break;
+                }
+            }
+            while (happy);
+            Console.Read();
+        }
+
+        while (okay)
+        {
+            Console.WriteLine("If you were graded on your feelings, you'd get a C.");
+            Console.Read();
+            break;
+        }
+
+        while (miserable)
+        {
+            Console.WriteLine("You have failed. Go play in the road :( ");
+            Console.Read();
+        }
+    }
+}
