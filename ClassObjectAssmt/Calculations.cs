ab07b52f7d6fab46c759157c02b7db809b6bc6b2
T33mac <31869841+T33mac@users.noreply.github.com>
2020-02-27 15:24:18 -0800

    checking code
    checking code

---
+++ ClassObjectAssmt/Calculations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjectAssmt
+{
+    public class Calculations
+    {
+        public Calculations()
+        {
+            Program Val = new Program();
+            int newNum = Convert.ToInt32(Val.nummy);
+            int ans1 = newNum / 5;
+            int ans2 = newNum + 13;
+            int ans3 = newNum * 2;
+        }
+        public int ans1 { get; set; }
+        public int ans2 { get; set; }
+        public int ans3 { get; set; }
+    }
+}
---
+++ ClassObjectAssmt/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjectAssmt
+{
+    public class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Calculations calc = new Calculations();
+            Console.WriteLine("Please enter a whole number in decimal form: ");
+            string num1 = Console.ReadLine();
+            int line1 = calc.ans1;
+            int line2 = calc.ans2;
+            int line3 = calc.ans3;
+            Console.WriteLine("Your number divided by five is: " + line1 + "\n\nYour number added to 13 is: " + line2 + 
+                "\n\nYour number doubled is: " + line3);
+            Console.ReadLine();
+        }
+        public Program(string num1)
+        {
+            string nummy = num1;
+        }
+        public string nummy { get; set; }
+    }
+}
