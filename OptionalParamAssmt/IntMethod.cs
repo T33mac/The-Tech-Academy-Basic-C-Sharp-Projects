581e5c59c101c51b12d7fac607b9830145263e2d
T33mac <31869841+T33mac@users.noreply.github.com>
2020-02-28 12:52:34 -0800

    add lms p.160 assignment
    add lms p.160 assignment

---
+++ OptionalParamAssmt/IntMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParamAssmt
+{
+    public class IntMethod
+    {
+        public int DoThis(int num1, int num2 = 4)
+        {
+            int math1 = num1 * num2;
+            return math1;
+        }
+    }
+}
---
+++ OptionalParamAssmt/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParamAssmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntMethod calc = new IntMethod();
+
+            Console.WriteLine("Enter a whole number in decimal form, press enter, " +
+                "\nthen you have the option to enter a second number. Press \nEnter to pass.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            string taco = Console.ReadLine();
+
+            if (taco.Length == 0)
+            {
+                int ans1 = calc.DoThis(num1);
+                Console.WriteLine("Your number times four is: {0}", ans1);
+            }
+            
+            else
+            {
+                int num2 = Convert.ToInt32(taco);
+                int ans2 = calc.DoThis(num1, num2);
+                Console.WriteLine("Your first number times your second number is: {0}", ans2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
