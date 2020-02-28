66a15103f367cc900bcbfb16f6cbf7acacb92ba3
T33mac <31869841+T33mac@users.noreply.github.com>
2020-02-28 10:30:29 -0800

    add lms p.159 assignment
    add lms p.159 assignment

---
+++ MethodOverloadingAssmt/Mathy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingAssmt
+{
+    public class Mathy
+    {
+        public int Calculate(int num1)
+        {
+            int math1 = num1 + 55;
+            return math1;
+        }
+        public int Calculate(decimal num2)
+        {
+            decimal mathy = num2 / 14;
+            int math2 = Convert.ToInt32(mathy);
+            return math2;
+        }
+        
+        public int Calculate(string num3)
+        {
+            int math3 = Convert.ToInt32(num3) * 2;
+            return math3;
+        }
+    }
+}
---
+++ MethodOverloadingAssmt/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingAssmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathy calc = new Mathy();
+
+            Console.WriteLine("Please enter a whole number in decimal form: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            
+            int ans1 = calc.Calculate(num1);
+            Console.WriteLine("\nLucky you! You've added fifty-five to your number. It is now: {0}.", ans1);
+
+            Console.WriteLine("\nPlease enter another whole number in decimal form: ");
+            decimal num2 = Convert.ToDecimal(Console.ReadLine());
+
+            int ans2 = calc.Calculate(num2);
+            Console.WriteLine("\nYour number was divided by Fourteen. It is now: {0} (plus or minus).", ans2);
+
+            Console.WriteLine("\nOkay. Enter a third whole number in decimal form now: ");
+            string num3 = Console.ReadLine();
+
+            int ans3 = calc.Calculate(num3);
+            Console.WriteLine("\nYou've doubled to your number. It is now : {0}.", ans3);
+            Console.ReadLine();
+        }
+    }
+}
