c672d919290edc41fedc54a9e34de862c626f822
T33mac <31869841+T33mac@users.noreply.github.com>
2020-02-27 16:38:11 -0800

    Update Program.cs

--- ClassObjectAssmt/Program.cs
+++ ClassObjectAssmt/Program.cs
@@ -17,7 +17,7 @@ namespace ClassObjectAssmt
             int ans1 = calc.Calculate1(num1);  //Parameter must be passed
             int ans2 = calc.Calculate2(num1);
             int ans3 = calc.Calculate3(num1);
-            Console.WriteLine("Your number added to 13 is: " + ans1 + "\n\nYour number multiplied by two is: " + ans2 + "\n\n Your number divided by two is: " + ans3);
+            Console.WriteLine("\n\nYour number added to 13 is: " + ans1 + "\n\nYour number multiplied by two is: " + ans2 + "\n\n Your number divided by two is: " + ans3);
             Console.ReadLine();
         }
     }
