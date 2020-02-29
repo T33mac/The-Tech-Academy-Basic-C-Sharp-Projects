9e2a10916e1f73c149aa34c58832836f04a32d83
T33mac <31869841+T33mac@users.noreply.github.com>
2020-02-29 08:05:03 -0800

    add lms p.161 assignment
    add lms p.161 assignment

---
+++ VoidMethodAssmt/Numbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssmt
+{
+    public class Numbers
+    {
+        public void DoStuff()
+        {
+            int num1 = 12;
+            int num2 = 22;
+            int math1 = num1 * 15;
+            Console.WriteLine("The default first number times fifteen is {0}.", math1);
+            Console.WriteLine("The default second number is {0}.", num2);
+        }
+
+    }
+}
---
+++ VoidMethodAssmt/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers b = new Numbers(); //instantiate a new class instance
+            b.DoStuff();               //calling method
+            Console.ReadKey();         //this seems to work
+        }
+    }
+}
