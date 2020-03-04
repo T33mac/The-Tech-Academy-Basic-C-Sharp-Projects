b63e5d3c589b995aa1d79c01dc89d47a0881d0c7
T33mac <31869841+T33mac@users.noreply.github.com>
2020-03-04 14:01:29 -0800

    add lms p.179 assignment
    add lms p.179 assignment

---
+++ IQuitAssmt/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssmt
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            base.SayName();
+        }
+        public void Quit(Employee employee)
+        {
+            Console.WriteLine("\nThis employee has quit. Collect all of their belongings and burn them in one of those barrels like in the movies.");
+        }
+    }
+}
---
+++ IQuitAssmt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person emp = new Employee();
+            //emp.firstName = "Sample";
+            //emp.lastName = "Student";
+            //emp.SayName();
+            //Console.ReadLine();
+
+            Employee emp = new Employee();
+            emp.firstName = "Sample";
+            emp.lastName = "Student";
+            emp.SayName();
+            emp.Quit(emp);          //Calling on the quit function
+            Console.ReadLine();
+        }
+    }
+}
