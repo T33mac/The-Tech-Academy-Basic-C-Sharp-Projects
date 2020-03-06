0404933bcbc1926ee8d5b59bcde7f18a14261daf
T33mac <31869841+T33mac@users.noreply.github.com>
2020-03-05 16:55:51 -0800

    p.184
    p.184

---
+++ OperatorOloadAssmt/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperClassAssmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> lstEmp = new List<Employee>();
+            Employee emp = new Employee();
+            emp.FirstName = "Sample";
+            emp.LastName = "Student";
+            emp.Id = 12;
+            lstEmp.Add(emp);
+            Employee emp1 = new Employee();
+            emp1.FirstName = "Smart";
+            emp1.LastName = "Guy";
+            emp1.Id = 12;
+            lstEmp.Add(emp1);
+
+            foreach (var emps in lstEmp)
+            {
+                Console.WriteLine(emps.FirstName + " " + emps.LastName + " #" + emps.Id);
+            }
+            Console.Read();
+        }
+    }
+}
