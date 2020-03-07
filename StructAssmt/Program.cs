b9c4bd22f9f81889a5db2f395fa29d2fb277daf1
T33mac <31869841+T33mac@users.noreply.github.com>
2020-03-07 09:51:37 -0800

    lms p.197 assignment

---
+++ StructAssmt/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 2255;
+            Console.WriteLine("Amount is: " + number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
