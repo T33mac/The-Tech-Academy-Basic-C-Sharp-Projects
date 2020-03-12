9a32d295928594c13cd111de6b7cf5155a40f5ae
T33mac <31869841+T33mac@users.noreply.github.com>
2020-03-12 09:51:48 -0700

    add lms p. 218 assignment

---
+++ LoggingP218Assmt/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LoggingP218Assmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string text1 = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\trmcg\Logs\log1.txt", text1);
+            string text2 = File.ReadAllText(@"C:\Users\trmcg\Logs\log1.txt");
+            Console.WriteLine("You've entered \"{0}\". It is now logged.", text2);
+            Console.Read();
+        }
+    }
+}
