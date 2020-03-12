53153877dde1eca9b1bb7d3d2e206da54bb66513
T33mac <31869841+T33mac@users.noreply.github.com>
2020-03-12 14:12:36 -0700

    add p. 221 assignment

---
+++ DateTimeP_221Assmt/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeP_221Assmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.WriteLine("Enter a number in decimal form.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            DateTime future = now.AddHours(num1);
+            Console.WriteLine("Your number was hours in the future. Check it!\n\n\t {0}", future);
+            Console.ReadLine();
+        }
+    }
+}
