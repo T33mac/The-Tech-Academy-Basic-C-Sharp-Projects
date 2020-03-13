2672e81b2061f0ec3023196cde8cff2230be6988
T33mac <31869841+T33mac@users.noreply.github.com>
2020-03-13 14:33:32 -0700

    add lms p.235 assignment

---
+++ ExceptionsP_235Assmt/AgeException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsP_235Assmt
+{
+    public class AgeException : Exception
+    {
+        public AgeException() : base() { }
+        public AgeException(string message) : base(message) { }
+    }
+}
---
+++ ExceptionsP_235Assmt/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsP_235Assmt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            try
+            {
+                Console.WriteLine("How old are you?");
+                int age = Convert.ToInt32(Console.ReadLine());
+                bool okAge = age > 0;
+                if (!okAge)
+                {
+                    throw new AgeException();
+                }
+                DateTime yearBorn = now.AddYears(-age);
+                Console.WriteLine("Your were born in " + yearBorn.Year);
+                Console.ReadLine();
+                return;
+            }
+            catch (AgeException)
+            {
+                Console.WriteLine("Please enter a positive number greater than zero:");
+                int age = Convert.ToInt32(Console.ReadLine());
+                DateTime yearBorn = now.AddYears(-age);
+                Console.WriteLine("Your were born in " + yearBorn.Year);
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something you've entered is not working. Try using numbers only:");
+                int age = Convert.ToInt32(Console.ReadLine());
+                DateTime yearBorn = now.AddYears(-age);
+                Console.WriteLine("Your were born in " + yearBorn.Year);
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
