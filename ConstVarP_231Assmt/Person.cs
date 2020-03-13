a8690395678c49af000fd2a9c62087005030d37a
T33mac <31869841+T33mac@users.noreply.github.com>
2020-03-13 12:11:05 -0700

    add lms p.231 assignment

---
+++ ConstVarP_231Assmt/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarP_231Assmt
+{
+    public class Person
+    {
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public Person(string fname): this(fname, "Jones")
+        {
+            
+        }
+        public Person(string fname, string lname)
+        {
+            Fname = fname;
+            Lname = lname;
+        }
+    }
+}
---
+++ ConstVarP_231Assmt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVarP_231Assmt
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string fname = "Joey";
+            var lname = "Rodgers";
+            Person person = new Person(fname);
+            Console.WriteLine("*With default lname from Constructor chain* \n\"" + person.Fname + " " + person.Lname + "\"");
+
+            Person person1 = new Person(fname, lname);
+            Console.WriteLine("\n*With both varibles filled in* \n\"" + person1.Fname + " " + person1.Lname + "\"");
+
+            Console.ReadLine();
+        }
+    }
+}
